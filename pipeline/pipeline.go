e603281e3058f842d270eacf9cd8f9aaf4644c97
connorfoody <connorfoody>
2015-10-07 19:37:26 -0400

    added test pipe

--- pipeline/pipeline.go
+++ pipeline/pipeline.go
@@ -3,10 +3,69 @@ package pipeline
 import (
 	"github.com/opinionated/analyzer-core/alchemy"
 	"github.com/opinionated/analyzer-core/analyzer"
-	"github.com/opinionated/scraper-core/article"
+	"os"
 )
 
-func StartPipe(article scraper.Article) {
-	mainArticle := analyzer.BuildAnalyzable()
-	err := alchemy.GetTaxonomy(article.GetData(), &mainArticle.Taxonomys)
+// TODO: make error chans
+
+// filter by taxonomy, this is just a rought for now
+func FilterTaxonomy(top analyzer.Analyzable,
+	in chan analyzer.Analyzable) chan analyzer.Analyzable {
+
+	out := make(chan analyzer.Analyzable)
+	// get a chan with the taxonomys already loaded
+	withTaxonomy := LoadTaxonomy(in)
+
+	go func() {
+		for toAnalyze := range withTaxonomy {
+			// TODO: make a filtering function to score them
+			if toAnalyze.Taxonomys.Taxonomys[0].Label == top.Taxonomys.Taxonomys[0].Label {
+				out <- toAnalyze
+			}
+		}
+		close(out)
+	}()
+
+	return out
+}
+
+// Gets taxonomy data from file and puts it into the chan
+func LoadTaxonomy(in chan analyzer.Analyzable) chan analyzer.Analyzable {
+
+	out := make(chan analyzer.Analyzable)
+
+	names := make(chan string)
+	go func() {
+		files := openFile(names)
+		for toAnalyze := range in {
+			names <- toAnalyze.FileName + "_taxonomy.xml"
+			toRead := <-files
+			defer toRead.Close() // need to close b/c open file won't do it for you
+			err := alchemy.ToXML(toRead, &toAnalyze.Taxonomys)
+			if err != nil {
+				panic(err)
+			}
+			out <- toAnalyze
+		}
+		close(out)
+	}()
+
+	return out
+}
+
+// NOTE: does not close file, you will need to do that later
+func openFile(in chan string) chan *os.File {
+	out := make(chan *os.File)
+
+	go func() {
+		for name := range in {
+			file, err := os.Open(name)
+			if err != nil {
+				panic(err)
+			}
+			out <- file
+		}
+		close(out)
+	}()
+	return out
 }
--- pipeline/pipeline_test.go
+++ pipeline/pipeline_test.go
@@ -1,1 +1,100 @@
 package pipeline_test
+
+import (
+	"fmt"
+	"github.com/opinionated/analyzer-core/alchemy"
+	"github.com/opinionated/analyzer-core/analyzer"
+	"github.com/opinionated/pipeline/pipeline"
+	"os"
+	"testing"
+)
+
+var simpleTaxonomySet = []string{
+	"thisIsWhatEscalationLooksLike",
+	"isValdimirPutinTryingToTeachTheWestALessonInSyria",
+	"alabamaPutsUpMoreHurdlesForVoters",
+}
+
+func TestFilterTaxonomys(t *testing.T) {
+	articles := make(chan analyzer.Analyzable)
+
+	go func() {
+		for i := 1; i < len(simpleTaxonomySet); i++ {
+			article := analyzer.BuildAnalyzable()
+			article.FileName = "testData/simpleTaxonomy/" + simpleTaxonomySet[i]
+			article.Name = simpleTaxonomySet[i]
+			articles <- article
+		}
+		close(articles)
+	}()
+
+	top := analyzer.BuildAnalyzable()
+	top.FileName = "testData/simpleTaxonomy/" + simpleTaxonomySet[0]
+	file, err := os.Open(top.FileName + "_taxonomy.xml")
+	if err != nil {
+		panic(err)
+	}
+
+	err = alchemy.ToXML(file, &top.Taxonomys)
+	if err != nil {
+		panic(err)
+	}
+
+	filtered := pipeline.FilterTaxonomy(top, articles)
+	found := <-filtered
+	if found.Name != simpleTaxonomySet[1] {
+		t.Errorf("expected article:", simpleTaxonomySet[1], "got:", found.Name)
+	}
+}
+
+func TestLoadTaxonomys(t *testing.T) {
+	articles := make(chan analyzer.Analyzable)
+	withTaxonomy := pipeline.LoadTaxonomy(articles)
+
+	go func() {
+		for _, link := range simpleTaxonomySet {
+			article := analyzer.BuildAnalyzable()
+			article.FileName = "testData/simpleTaxonomy/" + link
+			articles <- article
+		}
+		close(articles)
+	}()
+
+	for i := 0; i < len(simpleTaxonomySet); i++ {
+		analyzable := <-withTaxonomy
+		fmt.Println("analyzable:", analyzable)
+	}
+}
+
+func TestBuildData(t *testing.T) {
+	t.Skip("only run this when you need to set up a new set")
+	// build a test set
+	articles := simpleTaxonomySet
+	path := "testData/simpleTaxonomy/"
+	for _, link := range articles {
+		article, err := alchemy.ParseArticle(path + link + ".txt")
+		if err != nil {
+			panic(err)
+		}
+
+		keywords := alchemy.Keywords{}
+		err = alchemy.GetKeywords(article, &keywords)
+		if err != nil {
+			panic(err)
+		}
+		err = alchemy.MarshalToFile(path+link+"_keywords.xml", keywords)
+		if err != nil {
+			panic(err)
+		}
+
+		taxonomy := alchemy.Taxonomys{}
+		err = alchemy.GetTaxonomy(article, &taxonomy)
+		if err != nil {
+			panic(err)
+		}
+		err = alchemy.MarshalToFile(path+link+"_taxonomy.xml", taxonomy)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
